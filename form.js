7872e2e2546682be5a96853727de6c34a621b1e4
Ashutosh Kumar <ashu.heretocode@gmail.com>
2020-10-04 23:24:33 +0530

    finished validate function-part(2)

--- form.js
+++ form.js
@@ -4,5 +4,13 @@ const regexPattern= {
     username: /^[a-zA-Z_][\d\w_]{5,}$/,
     phone: /^91-[987][0-9]{9}$/,
     password: /^[\w@#-$&]{10,15}$/,
-    email:/^[a-zA-Z_](\w\.)+@[a-zA-Z-]+\.([a-zA-Z]{3,8})(\.[a-zA-Z]{3,8})?$/   //me@mydomain.com.org
+    email:/^[a-zA-Z_]([\w\.])+@[a-zA-Z-]+\.([a-zA-Z]{3,8})(\.[a-zA-Z]{3,8})?$/   //me@mydomain.com.org
 };
+
+//Step-2 Create a validate function which returns true or false
+
+function validate(pattern,text){
+    return pattern.test(text);
+}
+
+// console.log(validate(regexPattern.password,'p@$$w0rd_123'))
