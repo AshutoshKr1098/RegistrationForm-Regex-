d80ccf4afa83228c7d2d802c5be55362b6523f07
Ashutosh Kumar <ashu.heretocode@gmail.com>
2020-10-04 23:20:20 +0530

    finished regex-part(1)

---
+++ form.js
@@ -0,0 +1,8 @@
+//step-1 Create all regular expressions and store them in an object. 
+
+const regexPattern= {
+    username: /^[a-zA-Z_][\d\w_]{5,}$/,
+    phone: /^91-[987][0-9]{9}$/,
+    password: /^[\w@#-$&]{10,15}$/,
+    email:/^[a-zA-Z_](\w\.)+@[a-zA-Z-]+\.([a-zA-Z]{3,8})(\.[a-zA-Z]{3,8})?$/   //me@mydomain.com.org
+};
